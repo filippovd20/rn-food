4d7125f81ef255aa162b834b628b4840e4edaf4b
Dmitry Filippov <filippovd20@gmail.com>
2020-06-29 00:07:26 +0200

    93 - Managing State

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -4,23 +4,28 @@ import { Feather } from '@expo/vector-icons';
 
 const SearchBar = () => {
     return <View style={styles.backgroundStyle}>
-        <Feather name='search' size={30} />
+        <Feather name='search' style={styles.iconStyle} />
         <TextInput style={styles.inputStyle} placeholder="Search" />
     </View>
 };
 
 const styles = StyleSheet.create({
     backgroundStyle: {
-        backgroundColor: 'lightgray',
+        backgroundColor: '#F0EEEE',
         height: 50,
         borderRadius: 5,
         marginHorizontal: 15,
-        flexDirection:'row'
+        flexDirection:'row',
+        marginTop: 10
     },
     inputStyle: {
-        borderColor: 'black',
-        borderWidth: 1,
-        flex:1
+        flex:1,
+        fontSize:18,
+    },
+    iconStyle: {
+        fontSize: 35,
+        alignSelf: 'center',
+        marginHorizontal: 15
     }
 });
 
