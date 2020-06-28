24ac915f2659731b7710873485e9b4090d79e5e5
Dmitry Filippov <filippovd20@gmail.com>
2020-06-29 01:41:17 +0200

    running an initial search

--- src/api/yelp.js
+++ src/api/yelp.js
@@ -1,9 +1,9 @@
-import axios from 'axios';
+import axios from "axios";
 
 export default axios.create({
-    baseURL: 'https://api.yelp.com/v3/businesses',
-    header: {
-        Authorization: 'Bearer yoss5kB1tYqu2CVtXNpbellaMnJH7HXsWeMaFqgct5wFpSkuScgAA2GP4Uhl4q08dFJmly9ZtWm85sDTPyrM9puBG1MES96kHNOAgXc6BLw2r9rfxxd3gts23wP5XnYx'
-    }
+  baseURL: "https://api.yelp.com/v3/businesses",
+  headers: {
+    Authorization:
+      "Bearer Y3Vd2xMfolQ1lrUVa_c-do9Te1vKRymLLWB0imj3k2SdKo0YL7EaTIL0jTgJlLmfCM0KuXxDFx7iwSD1-XVfpaSXfaWENKvUbnEehmoyKO2qsk9HtT8CZf8vxSL5XnYx",
+  },
 });
-
