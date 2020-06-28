4bdc88c6d4ffddddaf5ed11ec7cb07dbe54ba06f
Dmitry Filippov <filippovd20@gmail.com>
2020-06-29 01:49:07 +0200

    100 - the useeffect hook

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -8,12 +8,12 @@ const SearchScreen = () => {
   const [results, setResults] = useState([]);
   const [errorMessage, setErrorMessage] = useState("");
 
-  const searchApi = async () => {
+  const searchApi = async (searchTerm) => {
     try {
       const response = await yelp.get("/search", {
         params: {
           limit: 50,
-          term, //term alternatively
+          term: searchTerm, //term alternatively
           location: "san jose",
         },
       });
@@ -23,9 +23,12 @@ const SearchScreen = () => {
     }
   };
 
+  //call searchAPI when component is first rendered
+  //searchApi('pasta');
+
   return (
     <View>
-      <SearchBar term={term} onTermChange={setTerm} onTermSubmit={searchApi} />
+      <SearchBar term={term} onTermChange={setTerm} onTermSubmit={()=>{searchApi(term)}} />
       {errorMessage ? <Text>{errorMessage}</Text> : null}
       <Text>We have found {results.length} results</Text>
     </View>
