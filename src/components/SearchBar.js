2b2554ab10067d923f41678e69d66b6b7b7b3788
Dmitry Filippov <filippovd20@gmail.com>
2020-06-28 23:44:20 +0200

    90 - Displaying icons

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -2,11 +2,18 @@ import React from 'react';
 import {View,Text,StyleSheet} from 'react-native';
 
 const SearchBar = () => {
-    return <View>
+    return <View style={styles.background}>
         <Text>Search Bar</Text>
     </View>
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+    background: {
+        backgroundColor: 'lightgray',
+        height: 50,
+        borderRadius: 5,
+        marginHorizontal: 15
+    }
+});
 
 export default SearchBar;
