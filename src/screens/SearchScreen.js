c671d4c21bff48005bc8b7dae07c63cf98583e0a
Dmitry Filippov <filippovd20@gmail.com>
2020-06-29 10:50:38 +0200

    103 - Grouping Results

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -1,7 +1,8 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import { View, Text, StyleSheet } from "react-native";
 import SearchBar from "../components/SearchBar";
 import useResults from '../hooks/useResults';
+import ResultsList from '../components/ResultsList';
 
 const SearchScreen = () => {
   const [term, setTerm] = useState("");
@@ -18,6 +19,9 @@ const SearchScreen = () => {
       />
       {errorMessage ? <Text>{errorMessage}</Text> : null}
       <Text>We have found {results.length} results</Text>
+      <ResultsList title="Cost Effective"/>
+      <ResultsList title="Bit Pricier"/>
+      <ResultsList title="Big Spender"/>
     </View>
   );
 };
